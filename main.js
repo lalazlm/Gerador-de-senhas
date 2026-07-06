cb0d812dae2693a5b68cdc77f5571fbf52ca81fc
lalazlm <laura.zelma.alves@escola.pr.gov.br>
2026-07-06 07:58:16 -0300

    salvar

--- main.js
+++ main.js
@@ -69,10 +69,11 @@ function classficaSenha(tamanhoSenha){
     forcaSenha.classList.remove('fraca','media','forte');
     if(entropia > 57){
         forcaSenha.classList.add('forte');
-    } else if (entropia > 35 && entropia < 57 ){
+    } else if (entropia > 35 && entropia < 57 ) {
         forcaSenha.classList.add('media');
-    } else if (entropia <= 35)
+    } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
-
+    }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = 2**Math.floor(entropia)/((100e6*60*60*24));
+}
