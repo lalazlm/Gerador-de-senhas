8b6b9252445114119ce35591fa814579ab4defdd
lalazlm <laura.zelma.alves@escola.pr.gov.br>
2026-07-27 10:46:43 -0300

    salvar

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function geraSenha(){
 }
 
 function classficaSenha(tamanhoSenha){
-    let entropia = tamanhoSenha * Math.log2(alfabeto.length);
+    let entropia = tamanhoSenha * Math.log2(tamanhoSenha);
     forcaSenha.classList.remove('fraca','media','forte');
     if(entropia > 57){
         forcaSenha.classList.add('forte');
