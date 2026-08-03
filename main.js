634f0125a3e2a3484285d2413aa38701d668ceb6
lalazlm <laura.zelma.alves@escola.pr.gov.br>
2026-08-03 10:43:34 -0300

    salvar

--- main.js
+++ main.js
@@ -73,6 +73,8 @@ function classificaSenha(tamanhoAlfabeto){
     } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
     }
-    const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/((100e6*60*60*24)) + "dias para descobrir essa senha";
+  const valorEntropia = document.querySelector('.entropia');
+    if (valorEntropia) {
+        valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+    }
 }
